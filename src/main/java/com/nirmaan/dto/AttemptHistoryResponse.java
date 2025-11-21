b6ffb7243a542b0f583f03a17fa9ab6d283ec50e
nandhakumarN <nandhakumarnagaraj62@gmail.com>
2025-11-21 15:06:40 +0530

    with security

---
+++ src/main/java/com/nirmaan/dto/AttemptHistoryResponse.java
@@ -0,0 +1,19 @@
+package com.nirmaan.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AttemptHistoryResponse {
+	
+	private Long attemptId;
+	private String quizText;
+	private Integer score;
+	private Integer totalQuestions;
+	private Double percentage;
+	private String grade;
+	private LocalDateTime attemptDate;
+}
